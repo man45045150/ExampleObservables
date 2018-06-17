14fdad44605bef0655fd926987c61c0b7ccd98a1
Aumnuay Niranartkul <man4504510@hotmail.co.th>
2018-06-17 21:28:45 +0700

    Custom from scratch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
+
 import 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +14,39 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe((number:number)=>{
-      console.log(number);
+    //interval is automaticlly add some numbers
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe((number:number)=>{
+    //   console.log(number);
+    // });
+
+    //observable from scratch
+    //this bridge from obserable to observer.
+    const myObservable = Observable.create((observer:Observer<string>)=>{
+      setTimeout(() => {
+        //next is emit data package
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        //next is emit data package
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        //next is emit data package
+        // observer.error('this does not work');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        //next is emit data package
+        observer.next('third package');
+      }, 6000);
     });
+
+    myObservable.subscribe(
+      (data:string)=>{ console.log(data); },
+      (error:string)=>{console.log(error); },
+      ()=>{ console.log('completed'); }
+    );
   }
 
 }
